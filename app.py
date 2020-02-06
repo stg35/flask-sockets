16a9594a1e370072a7ec31f7eafc9f1b8fa98a28
Alex <alex30052003@icloud.com>
2020-02-06 18:13:30 +0300

    no message

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/<name>')
-def index(name=None):
-    return render_template('index.html', name=name)
-
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
