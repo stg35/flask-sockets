4a18e877b915de145fdb48797085be278cb14afc
Alex <alex30052003@icloud.com>
2020-02-22 17:39:05 +0300

    no message

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "key"
 
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('message')
+def handler_message(msg):
+    print('User: ' + msg['user'] + ' Message: ' + msg['message'])
+    emit('message_sent', {'message': msg['message'], 'user': msg['user']}, broadcast=True)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    socketio.run(app, debug=True)
